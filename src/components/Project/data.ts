0b0cf0f1a978f171754b3f15d742aee14c862c2e
janos870 <janos.balogh870@gmail.com>
2023-11-24 07:01:15 +0100

    adding new project on the project

--- src/components/Project/data.ts
+++ src/components/Project/data.ts
@@ -1,26 +1,26 @@
 import mernAuthVideo from "../../assets/mern-auth.mp4";
 import imageFinderVideo from "../../assets/imageFinderVideo.mp4"
+import todoProjectVideo from "../../assets/todoProjectVideo.mp4"
 
 export const data = [
   {
     name: "MERN AUTH",
     description:
-      "I developed this project in Mern Stack, what you need to know about this is a complete authentications page, we can use update and delete profile, there is also google auth to open and make the login more secure for the user.",
+    "This project was developed using the MERN stack and features a comprehensive authentication system, allowing users to update and delete their profiles. Additionally, Google authentication is integrated to enhance user login security and convenience.",
     video: mernAuthVideo,
     url: "https://mern-auth-zmyu.onrender.com"
   },
   {
     name: "IMAGE FINDER",
-    description:
-      "I have developed a web application for image retrieval, conceived as a practical exercise. The application, while maintaining simplicity, prioritizes functionality and utility. To achieve this, an external backend API has been integrated, enabling seamless image search capabilities within the application.",
+    description:"This web application, designed as a practical exercise, facilitates image searching with a focus on simplicity, functionality, and utility. Integration of an external backend API ensures seamless image search capabilities within the application.",
     video: imageFinderVideo,
     url: "https://janos870.github.io/Image-List/"
   },
   {
-    name: "MERN AUTH",
+    name: "TODO-LIST",
     description:
-      "I developed this project in Mern Stack, what you need to know about this is a complete authentications page, we can use update and delete profile, there is also google auth to open and make the login more secure for the user.",
-    video: mernAuthVideo,
-    url: ""
+      "This web app represents my first project, featuring a todo list built with Node.js, Express, and EJS templates. Prioritizing simplicity, it provides seamless task management functionality.",
+    video: todoProjectVideo,
+    url: "https://todolist-ltv6.onrender.com/"
   },
 ];
